01d9b7765c15211676b57c55f0238d66181bcc6d
ThamerMashni <thamer.mashni@tuwaiq.edu.sa>
2021-06-02 20:27:59 +0300

    before components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state ={
+      input: "",
+      list: []
+    }
+    this.addItem = this.addItem.bind(this)
+    this.accInput = this.accInput.bind(this)
+    this.clear = this.clear.bind(this)
+  }
+  addItem(){
+    this.setState({
+      list: [...this.state.list,this.state.input],
+      input: ""
+    })
+    document.getElementById('input').value = ""
+  }  
+  clear(){
+    this.setState({
+      list: []
+    })
+    document.getElementById('input').value = ""
+  }  
+  accInput(){
+    this.setState({
+      input: document.getElementById('input').value
+    })
+  }
+  
+  render() {
+    var items = this.state.list.map((value)=>{
+        return(
+          <li>{value}</li>
+        )
+    })
+
+    return (
+      <div>
+        <h1>To-Do List</h1>
+        <ul id="List">
+          {items}
+        </ul>
+        <input id="input" onChange={this.accInput} type="text"></input>
+        <button onClick={this.addItem}>Add Item</button>
+        <button onClick={this.clear}>Clear List</button>
+      </div>
+    )
+  }
 }
 
-export default App;
+
---
+++ src/ListItem.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export default class ListItem extends Component {
+    constructor(props){
+        super(props)
+        this.state ={
+            
+        }
+
+    }
+
+    // var items = 
+    render() {
+        return (
+
+            <li>
+                item
+            </li>
+        )
+    }
+}
