e953c9d9b42009d9629766cc75ba402cc02bbda9
Abdulmajeed Almaymuni <Abdulmajeed.Almaymuni@tuwaiq.edu.sa>
2021-06-02 15:59:51 +0300

    Added Project files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ToDoList from './ToDoList';
+
+function App() {
+  return (
+    <div className="container">
+      <ToDoList />
+    </div>
+  );
+}
+
+export default App;
