912bfb67e8b1c723d688cbf08d8b535e2707f652
shahad <shahad@test>
2021-06-03 01:23:49 +0300

    First Commit

---
+++ todolist/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Title from './Title';
+import ToDoList from './ToDoList';
+
+function App() {
+  return (
+    <div className="App">
+    <Title/>
+    <ToDoList/>
+    </div>
+  );
+}
+
+export default App;
