9dd0cf2853047a4bcbc652cce6b47a50946793f2
aalhaif <Abdullah.Alhaif@Tuwaiq.edu.sa>
2021-06-03 10:11:44 +0300

    Final Update

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
     var itemArray = this.state.items;
 
     if (this._inputElement.value !== "") {    
-      itemArray.unshift(
+      itemArray.push(
         {      
           text: this._inputElement.value,      
           key: Date.now()    
@@ -33,21 +33,62 @@ class App extends Component {
     e.preventDefault();
   }
 
+  componentDidMount(){
+    var c = 1;
+    if (c==1){
+      this.firstTime();
+    }
+    this.state.c=2;
+  }
+  
+  firstTime(){
+    var itemArray = this.state.items;
+    var count=1;
+    itemArray.push(
+      {      
+        text: "Apple",      
+        key: count    
+      },
+      {
+        text: "Orange",      
+        key: count 
+      },
+      {
+        text: "Banana",
+        key: count
+      }
+    );
+      this.setState({      
+        items: itemArray,
+        count: count+1    
+      });   
 
-  render() {    
+      this._inputElement.value = "";  
+      this.state.itemArray= [];
+    console.log(itemArray);   
+  }
+
+  resetItems(){
+    var itemArray = this.state.items;
+      this.setState({items: []});
+    console.log(itemArray);   
+  }
+  
+
+  render() {
     return (      
-      <div id="container" className="todoListMain">        
-        <div className="header">          
-          <form onSubmit={this.addItem}>            
-            <input ref={(a) => this._inputElement = a} 
+      <><div id="container" className="todoListMain">
+        <div className="header">
+          <form onSubmit={this.addItem}>
+            <input ref={(a) => this._inputElement = a}
               placeholder="enter task">
-            </input>            
-            <button type="submit">add</button>          
-          </form>        
+            </input>
+            <button type="submit">add</button>
+          </form>
         </div>
 
-        <Lists entries={this.state.items} />     
-      </div>    
+        <Lists entries={this.state.items} />
+      </div><button onClick={() => this.resetItems()}>Reset</button></>
     );  
   }
 } 
